e649ef68a0446ba70ea05c6d1244179f20b890e3
Mikulew <mikulew@gmail.com>
2021-09-28 10:18:34 +0200

    Add logic and UI of Internet connection status

---
+++ main.js
@@ -0,0 +1,27 @@
+const internetStatus = document.getElementById("status");
+
+
+function displayOnlineStatus() {
+  if (internetStatus.classList.contains("offline")) {
+    internetStatus.classList.remove("offline");
+  }
+  internetStatus.classList.add("online");
+  internetStatus.textContent = "Internet Connection Available";
+}
+
+function displayOfflineStatus() {
+  if (internetStatus.classList.contains("online")) {
+    internetStatus.classList.remove("online");
+  }
+  internetStatus.classList.add("offline");
+  internetStatus.textContent = "No Internet Connect";
+}
+
+if (window.navigator.onLine) {
+  displayOnlineStatus();
+} else {
+  displayOfflineStatus();
+}
+
+window.addEventListener("online", displayOnlineStatus);
+window.addEventListener("offline", displayOfflineStatus);
